aa9095b3ad8706afcf3c6b334da69651acf9ecda
Thomas Niebur <mail@thomas-niebur.de>
2019-09-09 22:47:36 +0200

    Improve aggregation with total values

--- crawler/aggregate_orders.py
+++ crawler/aggregate_orders.py
@@ -9,7 +9,16 @@ with open('orders.jl', 'r') as f:
         year =  obj['order_date'][:4]
         costs = obj['order_costs']
         if not year in years:
-            years[year] = 0.0
-        years[year] += costs
+            years[year] = {'costs': 0.0, 'count': 0}
+        years[year]['costs'] += costs
+        years[year]['count'] += 1
+total_count = 0
+total_costs = 0.0
 for year in sorted(years.keys()):
-    print('%s: %5.2f€' % (year, years[year]))
+    costs = years[year]['costs']
+    count = years[year]['count']
+    print('%s:  %4d Bestellungen, %8.2f€ Kosten' % (year, count, costs))
+    total_count += count
+    total_costs += costs
+print('-' * 42)
+print('gesamt %4d Bestellungen, %8.2f€ Kosten' % (total_count, total_costs))
